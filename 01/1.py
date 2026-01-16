736f66cb79aba65016a65ead29ba60bbf2c8da33
Eugenesf7 <eugene.kholodnyak777@gmail.com>
2026-01-16 19:57:28 +0200

    first hw

---
+++ 01/1.py
@@ -0,0 +1,13 @@
+
+import math
+from math import e, pi
+from math import pi
+from math import e as test
+
+a = 10
+
+sum_1 = a * math.e
+sum_2 = a * math.pi
+
+print("Sum 1:", sum_1)
+print("Sum 2:", sum_2)
