677a4db2dc038912fd7e6d54b16c9e39dd3bcb2c
Eugenesf7 <eugene.kholodnyak777@gmail.com>
2026-02-12 14:19:53 +0200

    HW_5.1. Variable name

--- HW_5/5.1/5.1.py
+++ HW_5/5.1/5.1.py
@@ -4,26 +4,21 @@ import keyword
 name = input("Enter variable name: ")
 
 def is_valid_variable(name):
-    # 1. Should not be empty
     if not name:
         return False
 
-    # 2. Should not be a reserved word
     if name in keyword.kwlist:
         return False
 
-    # 3. Should not start with a number
     if name[0].isdigit():
         return False
 
-    # 4. Only lowercase letters, numbers and _
     allowed = string.ascii_lowercase + string.digits + "_"
     for ch in name:
         if ch not in allowed:
             return False
 
-    # 5. No more than one _
-    if "__" in name:
+    if set(name) == {"_"} and len(name) > 1:
         return False
 
     return True
